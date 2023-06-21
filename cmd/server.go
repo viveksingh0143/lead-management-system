75da75c7b5200c3a9abd980a129f68798bce0c6a
Vivek Singh <viveksingh0143@gmail.com>
2023-06-22 01:05:40 +0530

    Basic Command Line Created

---
+++ cmd/server.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2023 Vivek Kumar <viveksingh0143@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "It will run a server as rest-api for the lead manangement system",
+	Long:  `It will run a server as rest-api for the lead manangement system`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("server called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
